9423b6c391bc51d9884a50e36e24acb3cf699bcf
Ranjeet <ranjeet3774@gmail.com>
2023-05-02 03:27:03 +0530

    Git inital commit

---
+++ JavaPrograms/src/javaprograms/HelloWorld.java
@@ -0,0 +1,8 @@
+package javaprograms;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWord");
+	}
+}
---
+++ JavaPrograms/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ranje
+ *
+ */
+module JavaPrograms {
+}
